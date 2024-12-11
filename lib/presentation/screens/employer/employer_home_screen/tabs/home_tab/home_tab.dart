91648def2351a078ceb33f79244b8b77e7b82915
Your Name <you@example.com>
2024-12-12 01:54:56 +0200

    Finished home tab ui

--- lib/presentation/screens/employer/employer_home_screen/tabs/home_tab/home_tab.dart
+++ lib/presentation/screens/employer/employer_home_screen/tabs/home_tab/home_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:job_finder_app/utils/app_colors.dart';
 import 'package:job_finder_app/utils/app_styles.dart';
+import 'package:job_finder_app/utils/widgets/custom_button.dart';
 import 'package:job_finder_app/utils/widgets/custom_text_field.dart';
 
 class HomeTab extends StatelessWidget {
@@ -11,21 +12,76 @@ class HomeTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-      child: Column(
-        children: [
-          buildSearchContainer(),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            buildSearchContainer(),
+            const SizedBox(
+              height: 30,
+            ),
+            buildLatestJob(),
+            const SizedBox(
+              height: 30,
+            ),
+            buildRecentPeopleApplied(),
+          ],
+        ),
       ),
+    );
+  }
+
+  Column buildRecentPeopleApplied() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Recent people Applied',
+          style: AppStyle.titlesTextStyle,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        ListView.separated(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: 5,
+          itemBuilder: (context, index) => CustomJobAppliedAndDetails(),
+          separatorBuilder: (context, index) => SizedBox(
+            height: 10,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column buildLatestJob() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Your latest jobs',
+          style: AppStyle.titlesTextStyle,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        SizedBox(
+          height: 130,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 5,
+            itemBuilder: (context, index) => CustomJobCard(),
+          ),
+        ),
+      ],
     );
   }
 
   Container buildSearchContainer() {
     return Container(
       padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: const Color(0xFFF9F9FA),
-        borderRadius: BorderRadius.circular(20),
-      ),
+      decoration: AppStyle.containerDecoration,
       child: const Row(
         children: [
           Icon(
@@ -37,6 +93,187 @@ class HomeTab extends StatelessWidget {
           Text(
             'Search',
             style: AppStyle.labelStyle,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomJobAppliedAndDetails extends StatelessWidget {
+  const CustomJobAppliedAndDetails({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: AppStyle.containerDecoration,
+      child: Column(
+        children: [
+          const CustomJobAppliedCard(),
+          const Divider(
+            thickness: 1,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: CustomButton(
+                    title: 'See Details',
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomJobCard extends StatelessWidget {
+  const CustomJobCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+      width: MediaQuery.of(context).size.width * .89,
+      height: 110,
+      padding: const EdgeInsets.all(12),
+      decoration: AppStyle.containerDecoration,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(
+                  color: const Color(0xFFF9F9FA),
+                  width: 1,
+                ),
+              ),
+              child: Image.asset(
+                'assets/images/Icon.png',
+                height: 60,
+                width: 60,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 12,
+          ),
+          Expanded(
+            flex: 7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'UI/UX Designer',
+                  maxLines: 1,
+                  style: AppStyle.titlesJobTextStyle.copyWith(),
+                ),
+                Text(
+                  'Twitter',
+                  maxLines: 1,
+                  style: AppStyle.labelStyle.copyWith(color: Color(0xff394452)),
+                ),
+                Text(
+                  'On site - Full time',
+                  maxLines: 1,
+                  style: AppStyle.labelStyle,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomJobAppliedCard extends StatelessWidget {
+  const CustomJobAppliedCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+      width: MediaQuery.of(context).size.width * .89,
+      height: 110,
+      padding: const EdgeInsets.all(12),
+      decoration: AppStyle.containerDecoration,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(
+                  color: const Color(0xFFF9F9FA),
+                  width: 1,
+                ),
+              ),
+              child: Image.asset(
+                'assets/images/Icon.png',
+                height: 60,
+                width: 60,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 12,
+          ),
+          Expanded(
+            flex: 5,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Hassan mu',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: AppStyle.titlesJobTextStyle.copyWith(),
+                ),
+                Text(
+                  'Ui / ux designer',
+                  maxLines: 1,
+                  style: AppStyle.labelStyle.copyWith(color: Color(0xff394452)),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            width: 40,
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              decoration: BoxDecoration(
+                color: AppColors.primary.withOpacity(0.15),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Iconsax.message_2,
+                  size: 25,
+                ),
+              ),
+            ),
           ),
         ],
       ),
--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -1,18 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:job_finder_app/utils/app_colors.dart';
 
-
 abstract class AppStyle {
   static const TextStyle appBarStyle = TextStyle(
       fontSize: 30, fontWeight: FontWeight.bold, color: AppColors.white);
   static const TextStyle titlesTextStyle = TextStyle(
-      fontSize: 24, fontWeight: FontWeight.w800,color: Color(0xff15141F),fontFamily: 'fontBold');
+      fontSize: 24,
+      fontWeight: FontWeight.w800,
+      color: Color(0xff15141F),
+      fontFamily: 'fontBold');
 
   static const TextStyle subTitlesTextStyle = TextStyle(
-      fontSize: 18, fontWeight: FontWeight.w500,color: Color(0xffA2A0A8),fontFamily: 'fontBold');
+      fontSize: 18,
+      fontWeight: FontWeight.w500,
+      color: Color(0xffA2A0A8),
+      fontFamily: 'fontBold');
 
   static const TextStyle labelStyle = TextStyle(
-      fontSize: 16, color: Color(0xff717171),fontFamily: 'fontRegular');
+      fontSize: 16, color: Color(0xff717171), fontFamily: 'fontRegular');
+
+  static const TextStyle titlesJobTextStyle = TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w800,
+      color: Color(0xff15141F),
+      fontFamily: 'fontBold');
+
+  static BoxDecoration containerDecoration = BoxDecoration(
+    borderRadius: BorderRadius.circular(20),
+    border: Border.all(
+      color: const Color(0xFFF9F9FA),
+      width: 1,
+    ),
+    boxShadow: [
+      BoxShadow(
+        color: AppColors.primary.withOpacity(0.1),
+        offset: const Offset(
+          0.0,
+          0.0,
+        ),
+        blurRadius: 10.0,
+        spreadRadius: 2.0,
+      ), //BoxShadow
+      const BoxShadow(
+        color: Colors.white,
+        offset: const Offset(0.0, 0.0),
+        blurRadius: 0.0,
+        spreadRadius: 0.0,
+      ), //BoxShadow
+    ],
+  );
 
   static const TextStyle bottomSheetTitle = TextStyle(
       fontSize: 18, fontWeight: FontWeight.w700, color: AppColors.black);
